4215dc9dca71e24d94b6d392a9f12bf2fc70623c
Jeff Crowe <jeff@jeffjcrowe.com>
2016-12-30 21:23:18 -0800

    doc

--- src/client/faq.js
+++ src/client/faq.js
@@ -39,11 +39,11 @@ module.exports = React.createClass({
           <h3>I'm encryption savvy, what are you really doing?</h3>
           <ul>
             <li>A 256 bit random salt is generated for each file</li>
-            <li>Your password is hashed with the salt using a PBKDF2 128 iteration hash to generate a 256 bit encryption key</li>
+            <li>Your password is hashed with the salt using a PBKDF2 2000 iteration hash to generate a 256 bit encryption key</li>
             <li>The encryption key is hashed with SHA256 to allow verification of the correct key before download</li>
             <li>The original filename is encrypted with AES256-OFB-PKCS7 to allow download with the same name</li>
             <li>Each file is encrypted with AES256-OFB-NoPadding during multi-part upload by JavaScript (in browser)</li>
-            <li>The salt, hash of the key, and encrypted filename are included on the uploaded file as metadata</li>
+            <li>The salt, hash of the key, number of iterations, and encrypted filename are included on the uploaded file as metadata</li>
           </ul>
           <p>
             With the information here, a particularly savvy user could download their own .enc file and decrypt it themselves (but would still need the password).
